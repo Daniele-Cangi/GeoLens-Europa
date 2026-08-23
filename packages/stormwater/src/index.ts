98ab4210b431eafd2af0dd78b9fc8c9280430bd5
Daniele Cangi <dacangs@hotmail.it>
2026-08-23 13:11:50 +0200

    feat: condition Amsterdam surface catchment proxy

--- packages/stormwater/src/index.ts
+++ packages/stormwater/src/index.ts
@@ -1,6 +1,7 @@
 export * from './amsterdam';
 export * from './amsterdam-wfs';
 export * from './catchment';
+export * from './conditioned-surface-catchment';
 export * from './geojson';
 export * from './infrastructure';
 export * from './network';
